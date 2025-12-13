d584d0af72f2204f23350a28a456aad7902e7de4
dinhlamnamryul-sys <dinhlamnamryul@gmail.com>
2025-12-13 08:53:17 +0700

    Update 1_Gia_Sư_Toán_AI.py

--- pages/1_Gia_Sư_Toán_AI.py
+++ pages/1_Gia_Sư_Toán_AI.py
@@ -10,7 +10,8 @@ if not os.getenv("GOOGLE_API_KEY"):
     st.stop()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.0-pro")
+
 
 # ================== TRANG ==================
 st.set_page_config(
